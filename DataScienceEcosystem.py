1c59d8f796f7f9e34e9a4f8fc4381c8eb5e27321
pushkargarg04-droid <pushkargarg04@gmail.com>
2025-08-10 11:35:08 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # H1 : Data Science Tools And Ecosystem
+# 
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# __Objectives:__
+# *Languages used by data science
+# *Library used by data science
+# *Athmatic expression
+
+# Some of the popular languages that Data Scientists use are:
+
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Scala
+# 
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1. Panadas
+# 2. NumPy
+# 3. pyCharm
+# 4. GGplot2
+
+# |Data Science Tools|
+# |------------------:|
+# |Apache Hadoop|
+# |Apache Spark|
+# |Apache Hive|
+
+# ### H3 : Below are a few examples of evaluating arithmetic expressions in Python
+
+# [](: " This a simple arithmetic expression to mutiply then add integers")
+
+# In[4]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[9]:
+
+
+# This will convert 200 minutes to hours =
+200/60 
+
+
+# ## H2 : Author:
+#          Pushkar Garg
+
+# In[ ]:
+
+
+
+
